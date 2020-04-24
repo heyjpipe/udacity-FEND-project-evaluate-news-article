33086eeb43a55bb37e775c87e40cc54565a2e680
Jennifer PIper <52689372+heyjpipe@users.noreply.github.com>
2020-04-24 11:49:32 -0700

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,39 @@
-function handleSubmit(event) {
-    event.preventDefault()
+  function handleSubmit(event) {
+    event.preventDefault();
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
+    console.log("formText = ", formText)
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
+    if (Client.checkForName(formText)==true){
+    
+        fetch('http://localhost:8081/aylien',{
+         method: "POST",
+         credentials: "same-origin",
+         headers: {
+             'Content-Type': "application/json"
+         },
+         body: JSON.stringify({url:formText})
+     })
+     .then(res => res.json())
+     .then(function(res) {
+         document.getElementById('polarity').innerHTML = res.polarity
+         document.getElementById('subjectivity').innerHTML = res.subjectivity
+         document.getElementById('polar-conf').innerHTML = res.polarity_confidence
+         document.getElementById('subj-conf').innerHTML = res.subjectivity_confidence
+     })
+     }
+     else {
+         alert("Please try again.")
+         console.log("invlaid URL")
+         document.getElementById('message-area').innerText = "URL invalid. Try again.";
+     }
+     // console.log("::: Form Submitted :::")
+     // fetch('http://localhost:8081/test')
+     // .then(res => res.json())
+     // .then(function(res) {
+     //     document.getElementById('results').innerHTML = res.message
+     // })
+ }
 
 export { handleSubmit }
